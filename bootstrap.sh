66d635983f9dd3365cf224914db9d2abd3728cf4
Darian Culver <culver.darian@gmail.com>
2024-06-22 12:25:21 +1200

    Add handling for distro variants to manage software versioning issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,25 @@
 
 set -e
 
-GIT_VERSION="1:2.39.2-1.1"
-PYTHON_VENV_VERSION="3.11.2-6"
-CURL_VERSION="7.88.1-10+deb12u5"  # This SHOULD already be installed.
+echo -e \ "\nbootstrap.sh: A slightly hacky script for system provisioning.\n"
+
+# Check release
+apt-get update && apt-get install --assume-yes --no-install-recommends \
+    --auto-remove --quiet lsb-release
+
+DIST_CODENAME=$(lsb_release --short --codename 2>/dev/null)
+
+# FIXME - room for improvement using cURL to have distro specific version files,
+# similar to how we're dealing with Ansible requirements.
+if [ "${DIST_CODENAME}" == "bookworm" ]; then
+    echo -e "\nSetting up package versions for Debian Bookworm...\n"
+    GIT_VERSION="1:2.39.2-1.1"
+    PYTHON_VENV_VERSION="3.11.2-6"
+    CURL_VERSION="7.88.1-10+deb12u5"  # This SHOULD already be installed.
+else
+    echo -e "\nError! Current distro is not supported!\n"
+    exit 1
+fi
 
 BASE_REPOSITORY_URL="https://raw.githubusercontent.com/Darianisak/provisioning"
 GIT_BRANCH="darianculver/bootstrap-script"
