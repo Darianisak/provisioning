3717841b6cbf525fae5a9b197d19a2a0f43d45bc
Darian Culver <culver.darian@gmail.com>
2024-07-06 00:41:46 +1200

    Make the default user more specific to my usecase, _but_ also make it easier to update, both in Ansible, and in bootstrap.sh

--- integration-tests/interactive-container.sh
+++ integration-tests/interactive-container.sh
@@ -13,7 +13,7 @@ DOCKER_IMAGE="debian:bookworm"
 DOCKER_BASH_PATH="/usr/bin/bash"
 SYSTEM_MOUNT_POINT="/home/$USER/code/provisioning"
 DOCKER_MOUNT_POINT="/home/code"
-TEST_USER="www-data"
+TEST_USER="darianculver"
 
 docker run --user root --interactive --tty \
     --volume "${SYSTEM_MOUNT_POINT}:${DOCKER_MOUNT_POINT}" \
